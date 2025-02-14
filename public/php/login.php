8583a879c82a5e86a0618cfb4e38642c57c2dddf
joychien86 <joy860618@gmail.com>
2025-02-14 16:35:15 +0800

    修好登入功能、member 頁

--- public/php/login.php
+++ public/php/login.php
@@ -1,15 +1,18 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Content-Type: application/json; charset=utf-8');
+// header('Access-Control-Allow-Origin: http://localhost:3000');
+// header('Access-Control-Allow-Credentials: true');
+// header('Access-Control-Allow-Methods: POST');
+// header('Access-Control-Allow-Headers: Content-Type');
+// header('Content-Type: application/json; charset=utf-8');
+include('conn.php');
 
+session_start();
 $login =  json_decode(file_get_contents("php://input"), true);
 $email = htmlspecialchars($login['email']); //防止注射攻擊
 $password = $login['password'];
 
-include('connect.php');
+// include('connect.php');
+
 
 $sql = "SELECT password FROM G2_MEMBER WHERE email = ?";  
 $statement = $pdo->prepare($sql);  
@@ -22,7 +25,6 @@ $data = $statement->fetch(PDO::FETCH_ASSOC);
 if ($data) {
     $hashedPassword = $data['password'];
     if(password_verify($password, $hashedPassword)){
-        session_start();
         $_SESSION['login'] = true;
         $_SESSION['email'] = $email;
         echo json_encode([
